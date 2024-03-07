4db0b58d0d2735860459b8cacbd6cc21bf8f5df4
kangpahul9 <pahulpreet2959@gmail.com>
2024-03-07 10:58:40 +1030

    err14

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 void print_scaled(int array[3][3],int scale)
 {
+    int temp;
+    for (int i=0; i<scale; i++) {
+        for (int j=i+1; j<scale; j++) {
+            temp=array[i][j];
+            array[i][j]=array[j][i];
+            array[j][i]=temp;
+        }
+    }
     for (int i=0; i<scale; i++) {
         for (int j=0; j<scale; j++) {
-            std::cout<<array[j][i]<<" ";
+            std::cout<<array[i][j]<<" ";
         }
         std::cout<<'\n';
     }
--- main-1-4.cpp
+++ main-1-4.cpp
@@ -2,9 +2,9 @@ extern void print_scaled(int array[3][3],int scale);
 int main()
 {
     int scale=3;
-    int array[3][3]={{1,2,3},
-    {5,6,7},
-    {9,1,2},};
+    int array[3][3]={{7,0,0},
+    {0,7,651},
+    {0,0,0},};
     print_scaled(array,scale);
 ;
 }
